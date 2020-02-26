ad5e2faf99744119a198dc533ec0d3ce684f7b5a
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-02-26 12:21:28 -0600

    add functionality to pickup items while traversing graph

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import requests
 import random
 from datetime import datetime
-import time
 
 from player import Player
 from room import Room
-from apis import game_init, explore_new_room
+from apis import game_init, explore_room, pick_item, check_status, examine
+from utils import debounce
 
 reverse_direction = {"n": "s", "s": "n", "w": "e", "e": "w"}
 
@@ -59,25 +59,25 @@ def traverse(rooms, player, game_state):
 
         for direction in directions:
             print(f"Room count: {len(rooms)}")
-            time_now = datetime.now()
-            elapsed_time = time_now - game_state["last_call"]
-            cooldown = max(15, game_state["cooldown"])
-
-            if elapsed_time.seconds < cooldown:
-                delay = cooldown - elapsed_time.seconds
-                time.sleep(delay)
 
             current_room = player.current_room
-            response = explore_new_room(direction[0], direction[1])
-            game_state["last_call"] = datetime.now()
-            game_state["cooldown"] = response["cooldown"]
-            game_state["errors"] = response["errors"]
-            game_state["messages"] = response["messages"]
+            response = debounce(
+                explore_room,
+                game_state,
+                {"direction": direction[0], "room_id": direction[1]},
+            )
 
             room = Room(response)
             print(f"Current room: {room}")
             player.travel(room)
             record_move(rooms, room, current_room, direction[0])
+
+            # check for items
+            for item in response["items"]:
+                print("item: ", item)
+                response = debounce(examine, game_state, {"name": item})
+                response = debounce(pick_item, game_state, {"name": item})
+                response = debounce(check_status, game_state)
 
             print(game_state)
             print("------\n")
@@ -89,7 +89,12 @@ if __name__ == "__main__":
 
     # Game State
     # TODO: persist game state in a text file and load upon init
-    game_state = {"last_call": None, "cooldown": None, "errors": None, "messages": None}
+    game_state = {
+        "last_call": datetime.now(),
+        "cooldown": 0,
+        "errors": None,
+        "messages": None,
+    }
 
     # Rooms
     # TODO: persist room in a text file and load rooms from it
@@ -98,16 +103,19 @@ if __name__ == "__main__":
     if len(rooms.keys()) == 0:
 
         # Test API - initialize room
-        response = game_init()
-        game_state["last_call"] = datetime.now()
-        game_state["cooldown"] = response["cooldown"]
-        game_state["errors"] = response["errors"]
-        game_state["messages"] = response["messages"]
+        response = debounce(game_init, game_state)
 
         room = Room(response)
-        print(room)
+        print(f"Current room: {room}")
         player.play(room)
         record_move(rooms, room)
+
+        # check for items
+        for item in response["items"]:
+            print("item: ", item)
+            response = debounce(examine, game_state, {"name": item})
+            response = debounce(pick_item, game_state, {"name": item})
+            response = debounce(check_status, game_state)
 
     print(f"Please choose running mode:")
     print(f"1 - automatic traversal")
